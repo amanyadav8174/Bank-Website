ff6192fcf1701e77d754a224f52982ac0c7a4c33
aman yadav <amanyadav2508aa@gmail.com>
2024-12-26 13:52:51 +0530

    first commit

--- components/RecentTransactions.tsx
+++ components/RecentTransactions.tsx
@@ -1,8 +1,9 @@
-import Link from 'next/link'
+ import Link from 'next/link'
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 import { BankTabItem } from './BankTabItem'
 import BankInfo from './BankInfo'
 import TransactionsTable from './TransactionsTable'
+// import TransactionsTable from './TransactionsTable'
 import { Pagination } from './Pagination'
 
 const RecentTransactions = ({
